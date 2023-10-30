7d2c9e3489104cadff2ea339084410ede623bb96
Krizzis <alanasia904@gmail.com>
2023-10-30 15:23:04 +0300

    Ex1 Done

--- ex1/ex1.go
+++ ex1/ex1.go
@@ -1,7 +1,46 @@
+// Дана структура Human (с произвольным набором полей и методов).
+// Реализовать встраивание методов в структуре Action от
+// родительской структуры Human (аналог наследования)
+
 package main
 
 import "fmt"
 
+type Human struct {
+	name string
+	age  int
+}
+
+func (h *Human) hello() {
+	fmt.Printf("Hello! I`m %s, and I see u\n", h.name)
+}
+
+func (h *Human) goodbye() {
+	fmt.Println("See u!")
+}
+
+type Action struct {
+	Human //Встраивание родительской структуры Human
+}
+
 func main() {
-	fmt.Print("ex1")
+	fmt.Println("Задача 1")
+
+	h := Human{
+		name: "Alan",
+		age:  19,
+	}
+
+	a := Action{
+		Human: Human{name: "Ivan"},
+	}
+
+	h.hello()
+	a.hello()   // Вызов встроенного метода
+	a.goodbye() // Вызов встроенного метода
+
+	// Result:
+	// 		Hello! I`m Alan, and I see u
+	// 		Hello! I`m Ivan, and I see u
+	//		See u!
 }
